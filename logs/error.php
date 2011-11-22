617c2750aae6dc036a0a70173052cd285f5d1a7a
Anurag Anand <anurag.anand@tamarashoes.com>
2011-11-23 01:04:44 +0530

    Initial Commit.

---
+++ logs/error.php
@@ -0,0 +1,6 @@
+#<?php die('Forbidden.'); ?>
+#Date: 2011-11-22 14:47:20 UTC
+#Software: Joomla Platform 11.2.0 Stable+Modified [ Omar ] 27-Jul-2011 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2011-11-22	14:47:20	INFO	127.0.0.1	Joomla FAILURE: 	Invalid password
